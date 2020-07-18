e2e432195e3b110afe6d82dc4d1cd2aff3c86af8
Alex Doss <alex.doss42@gmail.com>
2020-07-18 13:40:37 -0600

    refactored components to their own files

---
+++ src/components/CreateExpense.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CreateExpense = () => {
+  <div>
+    Create Expense Page
+  </div>
+};
+
+export default CreateExpense;
---
+++ src/components/EditExpense.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const EditExpense = () => {
+  <div>
+    Edit Expense Page
+  </div>
+};
+
+export default EditExpense;
