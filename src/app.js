a1103fea7b4f1e6fdf87bd3f667dd67f4eb19801
Alex Doss <alex.doss42@gmail.com>
2020-07-18 10:42:11 -0600

    sets up a basic browserrouter

--- src/app.js
+++ src/app.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => {
+  <div>
+    This is from my dashboard component
+  </div>
+};
+
+const CreateExpensePage = () => {
+  <div>
+    Create Expense Page
+  </div>
+};
+
+const routes = (
+  <BrowserRouter>
+  <div>
+    <Route path='/' component={ExpenseDashboardPage} />
+    <Route path='/create' component={CreateExpensePage} />
+  </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
