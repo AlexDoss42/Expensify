dc431a48beb9b0427a722c2eabc5296ffbf6dda3
Alex Doss <alex.doss42@gmail.com>
2020-07-19 16:32:38 -0600

    adds redux for expensify to start that ball rollin

---
+++ src/playground/redux-expensify.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers } from 'redux';
+
+const demoState = {
+  expenses: [{
+    id: 'poijsdfas',
+    description: 'January Rent',
+    note: 'This was the final payment for 999 W',
+    amount: 39500,
+    createdAt: 0
+  }],
+  filters: {
+    text: 'rent',
+    sortBy: 'date', // date or amount
+    startDate: undefined,
+    endDate: undefined
+  }
+};
