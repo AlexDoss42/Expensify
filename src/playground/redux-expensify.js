bb0119f2011927fa6018c82ea5d8f505ba223210
Alex Doss <alex.doss42@gmail.com>
2020-07-24 21:22:45 -0600

    adds logic to filter with start date, end date, and text

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -116,13 +116,13 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
     default: 
       return state;
   }
-}
+};
 
 const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
   return expenses.filter((expense) => {
-    const startDateMatch;
-    const endDateMatch;
-    const textMatch;
+    const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+    const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
     return startDateMatch && endDateMatch && textMatch;
   });
