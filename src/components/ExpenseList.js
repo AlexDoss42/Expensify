a320c5bb6f40af27a49edb9f828b53d8e88fedc4
Alex Doss <alex.doss42@gmail.com>
2020-07-25 13:34:56 -0600

    adds sorted and filtered data

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import ExpenseListItem from './ExpenseListItem';
+import selectExpenses from '../selectors/expenses';
 
 const ExpenseList = (props) => (
   <div>
@@ -20,8 +21,7 @@ const ExpenseList = (props) => (
 
 const mapStateToProps = (state) => {
   return {
-    expenses: state.expenses,
-    filters: state.filters
+    expenses: selectExpenses(state.expenses, state.filters)
   };
 };
 
