dfb93e7ecb15cc3b6c4b12dd5d1fc105116ce825
Alex Doss <alex.doss42@gmail.com>
2020-07-25 13:05:19 -0600

    auth challenge

--- src/playground/hoc.js
+++ src/playground/hoc.js
@@ -17,6 +17,17 @@ const withAdminWarning = (WrappedComponent) => {
   )
 };
 
-const AdminInfo = withAdminWarning(Info);
+const requireAuthentication = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      {props.isAuthenticated && <p>This is classified info please don't share! Maverick will have to kill you if you do</p>}
+      <WrappedComponent {...props} />
+    </div>
+  )
+};
 
-ReactDOM.render(<AdminInfo isAdmin={false} info="There are the details" />, document.getElementById('app'));
+const AdminInfo = withAdminWarning(Info);
+const AuthInfo = requireAuthentication(Info);
+
+// ReactDOM.render(<AdminInfo isAdmin={false} info="There are the details" />, document.getElementById('app'));
+ReactDOM.render(<AuthInfo isAuthenticated={true} info="There are the details" />, document.getElementById('app'));
