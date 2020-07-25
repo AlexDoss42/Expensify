027f2ad3172e2059df86f52b4caeb18120460070
Alex Doss <alex.doss42@gmail.com>
2020-07-24 21:30:08 -0600

    adds sort logic

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -125,6 +125,14 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
     const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
     return startDateMatch && endDateMatch && textMatch;
+  }).sort((a, b) => {
+    if (sortBy === 'Date') {
+      return a.createdAt < b.createdAt ? 1 : -1;
+    } else if (sortBy === 'Amount') {
+      return a.amount < b.amount ? 1 : -1;
+    } else {
+      return
+    }
   });
 };
 
