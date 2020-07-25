920602c65addcefa2a0f8996359af0925b486f8b
Alex Doss <alex.doss42@gmail.com>
2020-07-25 13:23:20 -0600

    add filters to props

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,13 +4,15 @@ import { connect } from 'react-redux';
 const ExpenseList = (props) => (
   <div>
     <h1>EXPENSE LIST</h1>
+    {props.fitlers.text}
     {props.expenses.length}
   </div>
 );
 
 const mapStateToProps = (state) => {
   return {
-    expenses: state.expenses
+    expenses: state.expenses,
+    filters: state.filters
   };
 };
 
