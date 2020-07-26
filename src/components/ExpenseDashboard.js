1dc131053392205645381c309bf82a799ed3cb12
Alex Doss <alex.doss42@gmail.com>
2020-07-26 16:12:41 -0600

    adds basic filters component

--- src/components/ExpenseDashboard.js
+++ src/components/ExpenseDashboard.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
+import ExpenseListFitlers from './ExpenseListFilters';
 
 const ExpenseDashboard = () => {
   <div>
+    <ExpenseListFitlers />
     <ExpenseList />
   </div>
 };
