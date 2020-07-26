40e93ae02a3cff0ce1a640d0860c891be4aa7a37
Alex Doss <alex.doss42@gmail.com>
2020-07-26 16:54:06 -0600

    set up state and changes for note and description

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
 class ExpenseForm extends React.Component {
+  state = {
+    description: '',
+    note: ''
+  }
+
+  onDescriptionChange = (e) => {
+    const description = e.target.value;
+    this.setState(() => ({ description }));
+  }
+
+  onNoteChange = (e) => {
+    const note = e.target.value;
+    this.setState(() => ({ note }))
+  }
+
   render() {
     return (
       <div>
@@ -9,6 +24,8 @@ class ExpenseForm extends React.Component {
             type="text"
             placeholder="Description"
             autoFocus
+            value={this.state.description}
+            onChange={this.onDescriptionChange}
           />
           <input 
             type="number"
@@ -16,6 +33,8 @@ class ExpenseForm extends React.Component {
           />
           <textarea
             placeholder="Add a note for your expense (optional)"
+            value={this.state.note}
+            onChange={this.onNoteChange}
           >
           </textarea>
           <button>Add Expense</button>
