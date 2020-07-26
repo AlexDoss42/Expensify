2174dc31798cc2f896b82e6155e0ec168a15a666
Alex Doss <alex.doss42@gmail.com>
2020-07-26 16:39:42 -0600

    adds logic to sort by amount and date

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter } from '../actions/filters';
+import { setTextFilter, sortByAmount, sortByDate } from '../actions/filters';
 
 const ExpenseListFilters = (props) => (
   <div>
     <input type='text' value={props.filters.text} onChange={(e) => {
       props.dispatch(setTextFilter(e.target.value));
-    }}/>
+    }} />
+    <select 
+      value={props.fitler.sortBy}
+      onChange={(e) => {
+        if (e.target.value === 'Date') {
+          props.dispatch(sortByDate(e.target.value));
+        } else if (e.target.value === 'Amount') {
+          props.dispatch(sortByAmount(e.target.value));
+        }
+        return
+      }}
+    >
+      <option value="Date">Date</option>
+      <option value="Amount">Amount</option>
+    </select>
   </div>
 );
 
