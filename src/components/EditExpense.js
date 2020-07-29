b75c3bbe236d666c32011b16d31a3cd292897dae
Alex Doss <alex.doss42@gmail.com>
2020-07-28 21:31:13 -0600

    adds edit expense dispatch to on submit for edit page

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
+import { editExpense } from '../actions/expenses';
 
 const EditExpense = (props) => {
   <div>
     <ExpenseForm
       expense ={props.expense}
-      onSubmit={() => {
-
+      onSubmit={(expense) => {
+        props.dispatch(editExpense(props.expense.id, expense));
+        props.history.push('/');
       }}
     />
   </div>
