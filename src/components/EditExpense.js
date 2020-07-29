1c3997a2db1ab810155d6f2df22278074bcaf18c
Alex Doss <alex.doss42@gmail.com>
2020-07-28 21:20:27 -0600

    adds beginning logic to edit expense page

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const EditExpense = () => {
   <div>
@@ -6,4 +7,10 @@ const EditExpense = () => {
   </div>
 };
 
-export default EditExpense;
+const mapStateToProps = (state, props) => {
+  return {
+    expense: state.expense.find((expense) => expense.id === props.match.params.id)
+  };
+};
+
+export default connect(mapStateToProps)(EditExpense);
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
 
 export default ExpenseListItem = ({ dispatch, amount, description, createdAt, id }) => (
   <div>
-    <h3>{description}</h3>
-    <p>{amount}</p>
-    <p>{createdAt}</p>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
+    <p>{amount} - {createdAt}</p>
     <button onClick={() => {
       dispatch(removeExpense({ id }));
     }}>Remove</button>
