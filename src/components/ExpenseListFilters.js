1c1dea97a3d68781a090699e07320c42aebf3410
Alex Doss <alex.doss42@gmail.com>
2020-07-28 21:59:26 -0600

    set up state to allow for DateRangePicker

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { setTextFilter, sortByAmount, sortByDate } from '../actions/filters';
+import { DateRangePicker } from 'react-dates';
 
-const ExpenseListFilters = (props) => (
-  <div>
-    <input type='text' value={props.filters.text} onChange={(e) => {
-      props.dispatch(setTextFilter(e.target.value));
-    }} />
-    <select 
-      value={props.fitler.sortBy}
-      onChange={(e) => {
-        if (e.target.value === 'Date') {
-          props.dispatch(sortByDate(e.target.value));
-        } else if (e.target.value === 'Amount') {
-          props.dispatch(sortByAmount(e.target.value));
-        }
-        return
-      }}
-    >
-      <option value="Date">Date</option>
-      <option value="Amount">Amount</option>
-    </select>
-  </div>
-);
+const ExpenseListFilters = (props) => {
+
+  const [calendarFocused, setCalendarFocused] = useState(null);
+
+  return (
+    <div>
+      <input type='text' value={props.filters.text} onChange={(e) => {
+        props.dispatch(setTextFilter(e.target.value));
+      }} />
+      <select 
+        value={props.fitler.sortBy}
+        onChange={(e) => {
+          if (e.target.value === 'Date') {
+            props.dispatch(sortByDate(e.target.value));
+          } else if (e.target.value === 'Amount') {
+            props.dispatch(sortByAmount(e.target.value));
+          }
+          return
+        }}
+      >
+        <option value="Date">Date</option>
+        <option value="Amount">Amount</option>
+      </select>
+      <DateRangePicker
+
+      />
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
