b273c12f93af954c69aac1947cebf525500d05b6
Alex Doss <alex.doss42@gmail.com>
2020-07-30 21:01:55 -0600

    adds test for provided values add expense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -16,3 +16,24 @@ test('should set up edit expense action object', () => {
     updates: { note: 'New Note Value' }
   });
 });
+
+test('should set up add expense action object with provided values', () => {
+  const expenseData = {
+    description: 'Rent',
+    amount: 46500,
+    createdAt: 1000,
+    note: 'This was last months rent'
+  };
+  const action = addExpense(expenseData);
+  expect(action).toEqual({
+    type: 'ADD_EXPENSE',
+    expense: {
+      ...expenseData,
+      id: expect.any(String)
+    }
+  })
+});
+
+test('should set up add expense action object with default values', () => {
+
+});
