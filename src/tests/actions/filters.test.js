8d0d00ec17e0a478fb0f6f6668cfd83893ec6533
Alex Doss <alex.doss42@gmail.com>
2020-07-31 21:12:39 -0600

    adds tests for sort by date and amount

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -32,3 +32,17 @@ test('should generate set end date action object', () => {
     endDate: moment(0)
   });
 });
+
+test('should generate sort by amount action object', () =>{
+  const action = sortByAmount();
+  expect(action).toEqual({
+    type: 'SORT_BY_AMOUNT'
+  });
+});
+
+test('should generate sort by date action object', () =>{
+  const action = sortByDate();
+  expect(action).toEqual({
+    type: 'SORT_BY_DATE'
+  });
+});
