a2d1619a3ce9e9fb1dc522b73b7bfe5d8bb51c90
Alex Doss <alex.doss42@gmail.com>
2020-08-01 15:59:26 -0600

    adds test to add expense

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -1,6 +1,41 @@
 import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
 
 test('should set default state', () => {
   const state = expensesReducer(undefined, { type: '@@INIT' });
   expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: expense[1].id
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test('should not remove expenses if id not found', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: '-1'
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([expenses]);
+});
+
+test('should add an expense', () => {
+  const expense = {
+    id: '5',
+    description: 'Gas',
+    note: '',
+    amount: 500,
+    createdAt: 0
+  };
+  const action = {
+    type: "ADD_EXPENSE",
+    expense
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state.expenses[3]).toEqual(expense);
 });
