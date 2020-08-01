8770c24cd40d3475301ee317d14a25cb01df721d
Alex Doss <alex.doss42@gmail.com>
2020-07-31 21:32:55 -0600

    adds test for filter by end date, sort by date, sort by amount

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -46,3 +46,36 @@ test('should filter by StartDate', () => {
   const result = selectExpenses(expenses, filters);
   expect(result).toEqual([ expenses[2], expenses[0] ])
 });
+
+test('should filter by endDate', () => {
+  const filters = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: moment(0).subtract(1, 'day').valueOf
+  };
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([ expenses[1] ])
+});
+
+test('should sort by date', () => {
+  const filters = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+  };
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([ expenses[1], expenses[0], expenses[2] ]);
+});
+
+test('should sort by amount', () => {
+  const filters = {
+    text: '',
+    sortBy: 'amount',
+    startDate: undefined,
+    endDate: undefined
+  };
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([ expenses[1], expenses[2], expenses[0] ]);
+});
