fe740c7ebd749e472486153d6badf8be5e15d9f1
Alex Doss <alex.doss42@gmail.com>
2020-08-05 20:53:41 -0600

    adds a snapshot expect

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -5,5 +5,6 @@ import Header from '../../components/Header';
 test('should render the Header correctly', () => {
   const renderer = new ReactShallowRenderer();
   renderer.render(<Header />);
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
   console.log(renderer.getRenderOutput());
 });
