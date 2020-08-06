a2ad4cc310f3f4500b91ea48c9a3cdf3b627efb0
Alex Doss <alex.doss42@gmail.com>
2020-08-05 20:51:20 -0600

    writes first test on header renderer

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,3 +1,9 @@
 import React from 'react';
 import ReactShallowRenderer from 'react-test-renderer/shallow';
 import Header from '../../components/Header';
+
+test('should render the Header correctly', () => {
+  const renderer = new ReactShallowRenderer();
+  renderer.render(<Header />);
+  console.log(renderer.getRenderOutput());
+});
