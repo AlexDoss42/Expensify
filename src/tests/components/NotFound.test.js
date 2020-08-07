633e41046b0420e390fb41ff4a5d6ee548b7d57b
Alex Doss <alex.doss42@gmail.com>
2020-08-06 22:17:54 -0600

    fixes test name for notfound snapshot

--- src/tests/components/NotFound.test.js
+++ src/tests/components/NotFound.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import NotFound from '../../components/NotFound';
 
-test('should render the Header correctly', () => {
+test('should render the NotFound correctly', () => {
   const wrapper = shallow(<NotFound />);
   expect(wrapper).toMatchSnapshot();
 });
