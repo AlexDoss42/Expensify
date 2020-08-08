da4174e78717899ef8a84860fa4f7a64a4e31cf5
Alex Doss <alex.doss42@gmail.com>
2020-08-08 17:14:58 -0600

    refactors addexpenses to make it testable

--- src/components/CreateExpense.js
+++ src/components/CreateExpense.js
@@ -8,11 +8,15 @@ const CreateExpense = (props) => {
     <h1>Add Expense</h1>
     <ExpenseForm
       onSubmit={(expense) => {
-        props.dispatch(addExpense(expense));
+        props.onSubmit(expense);
         props.history.push('/');
       }}
     />
   </div>
 };
 
-export default connect()(CreateExpense);
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit: (expense) => dispatch(addExpense(expense))
+});
+
+export default connect(undefined, mapDispatchToProps)(CreateExpense);
