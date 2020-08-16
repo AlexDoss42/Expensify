9ab8a8dec5a26a3a697978a8941eed30fcff103d
Alex Doss <alex.doss42@gmail.com>
2020-08-16 15:59:33 -0600

    makes default path index.html

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(express.static(publicPath));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
 app.listen(3000, () => {
   console.log(`It's over Anakin. I have the 3000 port!`)
 });
