417681b69b8202244dffe053960222ddeafa0328
Alex Doss <alex.doss42@gmail.com>
2020-08-17 22:59:01 -0600

    adds function to pass test

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -2,6 +2,8 @@ export default (expenses) => {
  if(expenses.length === 0) {
    return 0;
  } else {
-   
+   return expenses.reduce((acc, curr) => {
+    return acc += curr.amount;
+   }, 0)
  }
 };
