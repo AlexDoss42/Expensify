592e031ffd8a3e0baa228e78f53b1553fa2ef98d
Alex Doss <alex.doss42@gmail.com>
2020-08-17 22:55:54 -0600

    tdd adds two more tests

--- src/tests/selectors/expenses-total.test.js
+++ src/tests/selectors/expenses-total.test.js
@@ -6,3 +6,12 @@ test('should return 0 if no expenses', () => {
   expect(res).toBe(0);
 });
 
+test('should correctly add up a single expense', () => {
+ const res = selectExpensesTotal([expenses[0]]);
+ expect(res).toBe(195);
+});
+
+test('should correctly add up multiple expenses', () => {
+  const res = selectExpensesTotal(expenses);
+  expect(res).toBe(11495);
+ });
