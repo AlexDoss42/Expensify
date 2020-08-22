88ee9516739abc1786cb4f4e660130f4f2e331ef
Alex Doss <alex.doss42@gmail.com>
2020-08-22 16:48:40 -0600

    adds a test to see if we send an expense to the database

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,5 +1,9 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
+
+const createMockStore = configureMockStore([thunk]);
 
 test('should set up remove expense action object', () => {
   const action = removeExpense({ id: '123' });
@@ -24,6 +28,31 @@ test('should set up add expense action object with provided values', () => {
     type: 'ADD_EXPENSE',
     expense: expenses[2]
   })
+});
+
+test('should add expense to database and store', (done) => {
+  const store = createMockStore({});
+  const expenseData = {
+    description: 'Mouse',
+    amount: 3000,
+    note: 'This one is better',
+    createdAt: 1000
+  }
+  store.dispatch(startAddExpense(expenseData)).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'ADD_EXPENSE',
+      expense: {
+        id: expect.any(String),
+        ...expenseData
+      }
+    });
+    done();
+  });
+});
+
+test('should add expense with defaults to database and store', () => {
+
 });
 
 // test('should set up add expense action object with default values', () => {
