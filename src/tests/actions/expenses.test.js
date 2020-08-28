db84f51c61260887342ce352cf31e996b07ce8dd
Alex Doss <alex.doss42@gmail.com>
2020-08-27 19:08:00 -0600

    adds test

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -2,6 +2,7 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
+import database from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk]);
 
@@ -47,7 +48,11 @@ test('should add expense to database and store', (done) => {
         ...expenseData
       }
     });
-    done();
+
+    database.ref(`expenses/${actions[0].expense.id}`).once('value').then((snapshot) => {
+      expect(snapshot.val()).toEqual(expenseData);
+      done();
+    });
   });
 });
 
