a90fbc5f19473618cb8e453f1cf446f352bcb200
Alex Doss <alex.doss42@gmail.com>
2020-09-07 16:42:48 -0600

    adds startLogin to login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { startLogin } from '../actions/auth';
 
-export const Login = () => (
+export const Login = ({ startLogin }) => (
   <div>
-    <button>Login</button>
+    <button onClick={startLogin}>Login</button>
   </div>
 );
+
+const mapDispatchToProps = (dispatch) => ({
+  startLogin: () => dispatch(startLogin())
+});
+
+export default connect(undefined, mapDispatchToProps)(Login);
