1e3afb68d0fa6203030f964933257409993b7083
Alex Doss <alex.doss42@gmail.com>
2020-09-09 18:00:19 -0600

    add redux login and logout to app.js

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from "./firebase/firebase";
 import { startSetExpenses } from './actions/expenses';
+import { login, logout } from './actions/auth';
 
 const store = configureStore();
 const jsx = (
@@ -27,6 +28,7 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged(() => {
   if (user) {
+    store.dispatch(login(user.uid));
     store.dispatch(startSetExpenses()).then(() => {
       renderApp();
       if (history.location.pathname === '/') {
@@ -34,6 +36,7 @@ firebase.auth().onAuthStateChanged(() => {
       }
     });
   } else {
+    store.dispatch(logout());
     renderApp();
     history.push('/');
   }
