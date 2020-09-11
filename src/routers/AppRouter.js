c4412bc7b273f46ce7a642700722b979af919fa7
Alex Doss <alex.doss42@gmail.com>
2020-09-10 22:57:30 -0600

    adds public route to avoid login and redirect to dashboard for logged in users

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -8,6 +8,7 @@ import Help from '../components/Help';
 import NotFound from '../components/NotFound';
 import Login from '../components/Login';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
@@ -15,7 +16,7 @@ const AppRouter = () => {
   <Router history={history}>
   <div>
     <Switch>
-      <Route path='/' component={Login} exact={true} />
+      <PublicRoute path='/' component={Login} exact={true} />
       <PrivateRoute path='/dashboard' component={ExpenseDashboard} />
       <PrivateRoute path='/create' component={CreateExpense} />
       <PrivateRoute path='/edit/:id' component={EditExpense} />
